8bbbeced29834dce8999862969348480e519227a
xiaomeng1989 <960184486@qq.com>
2019-12-16 16:21:07 +0800

    新建工具类，及要实现的方法

---
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -0,0 +1,26 @@
+package com.zeroten.common.util;
+
+public class CheckUtils {
+    //判断是否有空字符串
+    public static boolean isAnyEmpty(String... strings) {
+        return false;
+    }
+    //判断数组是否为空，长度为零或为null
+    public static boolean isEmpty(Object[] arr) {
+        return false;
+    }
+    //判断两个字符串是否相等
+    public static boolean equals(String str1,String str2) {
+        return false;
+    }
+    //判断两个整数是否相等
+    public static boolean equals(Integer n1, Integer n2) {
+        return false;
+    }
+
+
+
+
+
+
+}
